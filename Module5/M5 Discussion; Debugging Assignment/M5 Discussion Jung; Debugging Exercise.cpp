c485ab24df8e1cbcbc526716b5829adadc27bb05
Jae Jung <jjung@my365.bellevue.edu>
2024-04-11 21:54:01 -0700

    Adding M5 Discussion; Debugging Assignment

--- Module5/M5 Discussion; Debugging Assignment/M5 Discussion Jung; Debugging Exercise.cpp
+++ Module5/M5 Discussion; Debugging Assignment/M5 Discussion Jung; Debugging Exercise.cpp
@@ -1,6 +1,6 @@
 /*
 * Student Name: Jaeyun Jung
-* File Name: M4 Discussion; Debugging Assignment
+* File Name: M5 Discussion; Debugging Assignment
 * Date: 4/11/2024
 * 
 * This program will help users make a decision on which type of micro studio unit would be best to move into. 
@@ -9,30 +9,37 @@
 * The program will ask users to enter the amount of sq ft from the range 150-350 and then show the price of the unit. 
 * Then the program will display the total cost to live in the micro studio apartment for one year.
 * 
-* There are 2 errors! 
+* There are 2 errors!
+* 
 */
 
 #include <iostream>
 #include <conio.h>
 #include <iomanip>
 
-using namespace std; 
+using namespace std;
+
+double calculateCost(double monthly_rent)
+{
+    return monthly_rent * 12;
+
+}
 
 int main()
 {
 	cout << "Welcome to ABC Micro Studio Apartments. \n";
 	cout << "Please enter the desired square footage between 150 and 350 sq ft: ";
 
-	int sq_ft; 
-	cin >> sq_ft; 
+	int sq_ft;
+	cin >> sq_ft;
 
 	if (sq_ft < 150 || sq_ft > 350)
 	{
-		cout << "Sorry, this apartment does not have micro studio units of that size.\n"; 
+		cout << "Sorry, this apartment does not have micro studio units of that size.\n";
 	}
 	else
 	{
-		double monthly_rent; 
+		double monthly_rent;
 		if (sq_ft >= 150 && sq_ft <= 249)
 		{
 			monthly_rent = 1150.00;
@@ -40,7 +47,7 @@ int main()
 		}
 		else if (sq_ft >= 250 && sq_ft 299)
 		{
-			monthly_rent = 1400.00; 
+			monthly_rent = 1400.00;
 		}
 		else
 		{
@@ -54,16 +61,17 @@ int main()
 
 		cout << "The total monthly rent to live in this unit is $" << monthly_rent << ".\n";
 
-		double yearly_cost = monthly_rent * 12; 
+		double yearly_cost = calculateCost(monthly_rent);
 
 		cout << "The total yearly cost to live in this unit is $" << yearly_cost << ".\n";
 
-		cout << "\n Press any key to exit the program..." << endl; 
+		cout << "\n Press any key to exit the program..." << endl;
 
 	
 
-	_getch(); //Pause the program.
+	_getch(); //Pause the program
 
 	return 0; 
-
 }
+
+
