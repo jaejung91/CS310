95a3ab155ad76056be89d41c64b944142522064b
Jae Jung <jjung@my365.bellevue.edu>
2024-04-14 15:09:12 -0700

    adding M5 Discussion reply to Jelinek2Error

---
+++ Module5/M5 Discussion; Debugging Assignment/Solution to Jelinek2Error M5 Discussion.cpp
@@ -0,0 +1,42 @@
+/*
+* Errors fixed by: Jaeyun Jung
+* 
+* 
+* Author: Timothy Jelinek
+* Date: 4/11/2024
+* Description: This program has two user defined functions, one that adds and one that subtracts, two numbers. 
+* There are two errors in here to check for.
+*/
+
+#include <iostream>
+#include <conio.h> 
+using namespace std;
+
+// This is a function to add two numbers
+int add(int a, int b) {  //Error 1: Changed in to int - typo 
+    return (a + b);
+}
+
+// This is a function to subtract two numbers 
+int subtract(int a, int b) {
+    return (a - b);
+}
+
+int main(){ //Error 2: Added missing ) parentheses. 
+    // variables
+    int num1 = 289;
+    int num2 = 55;
+
+    // This calls the add function
+    int sum = add(num1, num2);
+    cout << "289 + 55 = " << sum << endl;
+
+    // This calls the subtract function
+    int difference = subtract(num1, num2);
+    cout << "289 - 55 = " << difference << endl;
+
+    cout << "\nPress any key to exit the program.";
+    _getch(); // pause the program so the user can see the output
+
+    return 0;
+    }
