db1535ce90742d080762ed086fa9b278bbff1ba0
Jae Jung <jjung@my365.bellevue.edu>
2024-05-05 01:29:43 -0700

    adding solution to nbujri M8 Discussion and other solution

---
+++ Module8/M8 Discussion; Debugging Assignment/M8 Discussion Solution; Jung Debugging Exercise.cpp
@@ -0,0 +1,95 @@
+/*
+* Student Name: Jaeyun Jung
+* File Name: M8 Discussion; Debugging Assignment
+* Date: 5/2/2024
+*
+* This program uses 2 classes: 'CarOwner' and 'Car' with car attributes such as brand, model, price,
+* and owner. The program creates instances of these classes to represent two cars
+* with their respective owners.
+* The program will then display information about each car.
+*
+* There are 2 errors!
+*
+*/
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <conio.h>
+
+using namespace std;
+
+class CarOwner
+{
+public:
+    string name;
+    int age;
+
+    CarOwner(string n, int a) : name(n), age(a) {}
+
+    //Function to display owner information. 
+    void displayOwner()
+    {
+        cout << "Owner Name: " << name << endl;
+        cout << "Owner Age: " << age << endl; //Error 1: Missing the "endl" after the cout
+
+    }
+};
+
+class Car
+{
+public:
+    string brand;
+    string model;
+    double price;
+    CarOwner owner; //Car has an owner 
+
+    Car(string b, string mo, double p, CarOwner o)
+        : brand(b), model(mo), price(p), owner(o) {}
+
+    //Function to display car information
+    void displayCar()
+    {
+        cout << "Car Brand: " << brand << endl;
+        cout << "Car Model: " << model << endl;
+
+        //Setting the locale for thousands separator. 
+        locale loc("");
+        cout.imbue(loc);
+
+        //Display price with the thousands separator and "$" sign 
+        cout << "Car Price: $" << fixed << setprecision(2) << price << endl;
+
+        cout << "Car Owner Information: " << endl;
+        owner.displayOwner(); //Display owner information 
+    }
+};
+
+
+int main()
+{
+    //Create instances of CarOwner
+    CarOwner owner1("Ashley", 30);
+    CarOwner owner2("Peter", 32);
+
+    //Create instances of Car
+    Car car1("Toyota", "Venza", 42000.00, owner1);
+    Car car2("Porsche", "Taycan", 99400.00, owner2);
+
+
+    //Display car information
+    cout << "Car 1 Information: " << endl;
+    car1.displayCar();
+    cout << endl;
+
+
+    cout << "Car 2 Information: " << endl;
+    car2.displayCar(); //Error 2: Since it is information for "Car 2", it should be "car2.displayCar()"
+
+
+    cout << "\n Press any key to exit the program..." << endl;
+
+    _getch(); //Pause the program 
+
+    return 0;
+}
---
+++ Module8/M8 Discussion; Debugging Assignment/Solution to nbujri M8 Discussion.cpp
@@ -0,0 +1,61 @@
+/*
+* Errors fixed by Jaeyun Jung 
+* for nbujri M8 Discussion 
+* 
+* M8Discussion.cpp : This file contains the 'main' function. Program execution begins and ends there.
+*/
+
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+class Dorm {
+public:
+    // constructor
+    Dorm(string name, int id) {
+        dormName = name;
+        dormID = id;
+    }
+
+    string dormName;
+    int dormID;
+};
+
+class Student {
+private:
+    string firstName;
+    string lastName;
+    Dorm dorm;
+
+public:
+    // constructor
+    Student(string first, string last, Dorm dorm) : firstName(first), lastName(last), dorm(dorm) {}
+
+    // display student information
+    void displayStudentInfo() {
+        cout << "\n  student name: " << firstName << " " << lastName << " || dorm: " << dorm.dormName << " (" << dorm.dormID << ")" << endl;
+    }
+};
+
+
+int main()
+{
+    // dorm objects
+    Dorm dorm1 = Dorm("North Dorm", 1);
+    Dorm dorm2 = Dorm("South Dorm", 2);
+
+    //student objects
+    Student student1 = Student("john", "doe", dorm1); //Error 1: included "dorm1" since there was a missing argument for student1
+    Student student2 = Student("jane", "doe", dorm2);
+
+    // display info for student 1 and 2
+    student1.displayStudentInfo();
+    student2.displayStudentInfo(); //Error 2: added missing parentheses after "displayStudentInfo" function call 
+
+    cout << "\n  press any key to continuee..." << endl;
+
+    _getch();
+    return 0;
+}
