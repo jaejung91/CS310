cba0a5b1a7eaf6d21a2df2e64f9077a0ca0fe244
Jae Jung <jjung@my365.bellevue.edu>
2024-05-09 21:49:52 -0700

    adding M9 Discussion post

--- Module9/M9 Discussion; Debugging Assignment/M9 Discussion Jung; Debugging Exercise.cpp
+++ Module9/M9 Discussion; Debugging Assignment/M9 Discussion Jung; Debugging Exercise.cpp
@@ -2,49 +2,51 @@
 * Student Name: Jaeyun Jung
 * File Name: M9 Discussion; Debugging Assignment
 * Date: 5/9/2024
+*
+* This program demonstrates inheritance with a base class and at least 2 child classes.
+* The program will ask users to input their skin type as "sensitive" or "normal".
+* Based on the user's input, the program will display which type of product will work best for the user's skintype.
 * 
-* This program demonstrates inheritance with a base class and at least 2 child classes. 
-* The program will ask users to input their skin type as "sensitive" or "normal". 
-* Based on the user's input, the program will 
+* There are 2 errors! Good luck! 
 */
 
 #include <iostream>
 #include <conio.h>
 #include <string>
 
-using namespace std; 
+using namespace std;
 
 //Base class SkincareProduct
 class SkincareProduct
 {
-protected: 
-    string type; 
-    string brand; 
-    string price; 
+protected:
+    string type;
+    string brand;
+    string price;
 
-public: 
+public:
     SkincareProduct(string ty, string br, double pr) : type(ty), brand(br), price(pr) {}
 
     void display()
     {
-        cout << "Product Type: " << type << "\n Brand: " << brand << "\n Price: $" << price << endl; 
+        cout << "Product Type: " << type << "\n Brand: " << brand << "\n Price: $" << price << endl;
     }
 };
 
 //Child Class: Nonsensitive Skincare
 class NonsensitiveSkincare : public SkincareProduct
 {
-private: 
-    string skinType; 
+private:
+    string skinType;
 
 public:
     NonsensitiveSkincare(string ty, string br, double pr, string skinty) : SkincareProduct(ty, br, pr), skinType(skinty) {}
 
     void display()
     {
-        cout << "Nonsensitive Skincare: "; 
-        SkincareProduct::display(); 
-        cout << "Suitable for: " << skinType << " skin" << endl; 
+        cout << "Nonsensitive Skincare: ";
+        SkincareProduct::display();
+        cout << "Suitable for: " << skinType << " skin" << endl;
     }
 };
 
@@ -52,7 +54,7 @@ public:
 class SensitiveSkincare : public SkincareProduct
 {
 private:
-    string sensitiveIngredient; 
+    string sensitiveIngredient;
 
 public:
     SensitiveSkincare(string ty, string br, double pr, string sensitiveI) : SkincareProduct(ty, br, pr), sensitiveIngredient(sensitiveI) {}
@@ -68,37 +70,36 @@ public:
 
 int main()
 {
-    string inputSkinType; 
+    string inputSkinType;
 
     //Prompt user to input their skin type
     cout << "Enter your skin type ('sensitive' or 'normal'): ";
     cin >> inputSkinType;
 
     //Create objects of child classes based on the user's input
-    SkincareProduct *skincareProduct; 
+    SkincareProduct* skincareProduct;
     if (inputSkinType == "sensitive")
     {
-        skincareProduct = new SensitiveSkincare("Essence", "Mixsoon", 29.99, "Centella Asiatica"); 
+        skincareProduct = new SensitiveSkincare("Essence", "Mixsoon", 29.99, "Centella Asiatica");
     }
     else if (inputSkinType == "normal")
     {
-        skincareProduct = new NonsensitiveSkincare("Serum", "Purito", 19.99, "Seawater"); 
+        skincareProduct = new NonsensitiveSkincare("Serum", "Purito", 19.99, "Seawater");
     }
     else
     {
-        cout << "Invalid input! Please enter 'sensitive' or 'normal' - all lowercase please!" << endl; 
-        return 1; 
+        cout << "Invalid input! Please enter 'sensitive' or 'normal' - all lowercase please!" << endl;
+        return 1;
     }
 
- 
+
     //Display information about the skincare product
     skincareProduct->display();
 
-    
-    cout << "\n Press any key to exit the program..." << endl; 
+
+    cout << "\n Press any key to exit the program..." << endl;
 
     _getch(); //Pause the program 
 
-    return 0; 
+    return 0;
 }
-
